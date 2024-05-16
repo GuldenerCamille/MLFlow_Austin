a6ab1ad366bebffe606d921b7d4bbc75b5cbee85
GULDENER Camille <98022298+GuldenerCamille@users.noreply.github.com>
2024-05-17 00:07:19 +0200

    modif port

--- run_mlflow.py
+++ run_mlflow.py
@@ -5,7 +5,10 @@ import mlflow
 os.makedirs("mlruns", exist_ok=True)
 artifact_root = os.path.abspath("mlruns")
 
+# Get the port from the environment variable
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == "__main__":
     mlflow.set_tracking_uri("sqlite:///mlflow.db")
     mlflow.set_registry_uri("sqlite:///mlflow.db")
-    mlflow.server(host='0.0.0.0', port=5000, backend_store_uri='sqlite:///mlflow.db', default_artifact_root=artifact_root)
+    mlflow.server(host='0.0.0.0', port=port, backend_store_uri='sqlite:///mlflow.db', default_artifact_root=artifact_root)
